06774df4ab0906fff65115513b46c4aa4af83206
YongjinKim <dydwls0669@gmail.com>
2020-09-23 22:11:45 +0900

    Security

--- WEB2 - Node.js/main.js
+++ WEB2 - Node.js/main.js
@@ -2,8 +2,8 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
-
 var template = require('./lib/template.js');
+var path = require('path');
 
 var app = http.createServer(function(request, response){
     var _url = request.url;
@@ -22,7 +22,8 @@ var app = http.createServer(function(request, response){
         })
       } else {
         fs.readdir('./data', function(error, filelist){
-          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var filteredId = path.parse(queryData.id).base;
+          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
             var title = queryData.id;
             var list = template.list(filelist);
             var html = template.html(title, list, `<h2>${title}</h2>${description}`,
@@ -72,7 +73,8 @@ var app = http.createServer(function(request, response){
       });
     } else if(pathname === '/update'){
       fs.readdir('./data', function(error, filelist){
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        var filteredId = path.parse(queryData.id).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
           var title = queryData.id;
           var list = template.list(filelist);
           var html = template.html(title, list, `
@@ -117,7 +119,8 @@ var app = http.createServer(function(request, response){
       request.on('end', function(){
         var post = qs.parse(body);
         var id = post.id;
-        fs.unlink(`data/${id}`, function(error){
+        var filteredId = path.parse(id).base;
+        fs.unlink(`data/${filteredId}`, function(error){
           response.writeHead(302, {Location: `/`});
           response.end();
         })
