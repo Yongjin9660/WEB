2bfc5c7542d97d7a16f67924fdeacf46716e9454
YongjinKim <dydwls0669@gmail.com>
2020-09-25 17:01:39 +0900

    mysql

--- Nodejs-Mysql/nodejs/mysql.js
+++ Nodejs-Mysql/nodejs/mysql.js
@@ -1,19 +1,18 @@
-var mysql      = require('mysql');
-// 비밀번호는 별도의 파일로 분리해서 버전관리에 포함시키지 않아야 합니다. 
+var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '111111',
-  database : 'opentutorials'
+    host    : 'localhost',
+    user    : 'root',
+    password: 'wjavm339563',
+    database: 'opentutorials'
 });
- 
+
 connection.connect();
- 
-connection.query('SELECT * FROM topic', function (error, results, fields) {
-    if (error) {
+
+connection.query('SELECT * FROM topic', function(error, results, fields) {
+    if(error) {
         console.log(error);
     }
     console.log(results);
 });
- 
-connection.end();
+
+console.groupEnd();
