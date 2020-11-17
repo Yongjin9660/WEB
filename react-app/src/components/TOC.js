7f01bfae65f47848acbb2fd3741c5620c297d8a6
YongjinKim <dydwls0669@gmail.com>
2020-11-18 00:12:31 +0900

    final

--- react-app/src/components/TOC.js
+++ react-app/src/components/TOC.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 
 class TOC extends Component {
-    render() {
-      return (
-        <nav>
-          <ul>
-            <li><a href="1.html">HTML</a></li>
-            <li><a href="1.css">CSS</a></li>
-            <li><a href="1.html">JavaScript</a></li>
-          </ul>
-        </nav>
-      );
+  shouldComponentUpdate(newProps, newState) {
+    if(this.props.data === newProps.data){
+      return false;
     }
+    return true;
   }
+  render() {
+    var lists = [];
+    var data = this.props.data;
+    var i = 0;
+    while (i < data.length) {
+      lists.push(<li key={data[i].id}>
+        <a
+          href={"/content/" + data[i].id}
+          data-id={data[i].id}
+          onClick={function (e) {
+            e.preventDefault();
+            this.props.onChangePage(e.target.dataset.id);
+          }.bind(this)}
+        >{data[i].title}</a>
+      </li>)
+      i++;
+    }
+    return (
+      <nav>
+        <ul>
+          {lists}
+        </ul>
+      </nav>
+    );
+  }
+}
 
   export default TOC;
